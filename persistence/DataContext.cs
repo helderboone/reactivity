aea147254dcddaf66c88f1828aa9f974a7259110
Helder Boone <helder.boone@gmail.com>
2019-12-13 10:56:04 -0300

    Create values entity dataseed

--- persistence/DataContext.cs
+++ persistence/DataContext.cs
@@ -8,5 +8,16 @@ namespace persistence
         public DataContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "Value 101" },
+                    new Value { Id = 2, Name = "Value 102" },
+                    new Value { Id = 3, Name = "Value 103" }
+                );
+
+        }
     }
 }
