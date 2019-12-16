efa7ed4bd7c55a99b98d6579303ffe1ca533c7d8
Helder Boone <helder.boone@gmail.com>
2019-12-16 15:04:50 -0300

    Reorganize react client app

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Header, Icon, List } from 'semantic-ui-react'
-import './App.css';
 import axios from 'axios';
 
 
 class App extends Component {  
   state = {
-    values: []
+    activities: []
   }
 
   componentDidMount(){
-    axios.get('http://localhost:5000/api/values')
+    axios.get('http://localhost:5000/api/activities')
       .then((response) => {
         this.setState({
-          values: response.data
+          activities: response.data
         })
       })    
   }
@@ -26,8 +25,8 @@ class App extends Component {
           <Header.Content>Reactivities</Header.Content>
         </Header>    
         <List>
-        {this.state.values.map((value: any) => (
-                <List.Item key={value.id}>{value.name}</List.Item>          
+        {this.state.activities.map((activity: any) => (
+                <List.Item key={activity.id}>{activity.title}</List.Item>          
               ))
             }          
         </List>
