12d348cda9888eef7802ac85e406e1d2ab75683f
Helder Boone <helder.boone@gmail.com>
2019-12-17 13:31:41 -0300

    Load form to edit an activity

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -35,7 +35,9 @@ const ActivityDashboard: React.FC<IProps> = ({
             setSelectedActivity={setSelectedActivity}
           />
         )}
-        {editMode && <ActivityForm setEditMode={setEditMode} />}
+        {editMode && (
+          <ActivityForm setEditMode={setEditMode} activity={selectedActivity!} />
+        )}
       </Grid.Column>
     </Grid>
   );
