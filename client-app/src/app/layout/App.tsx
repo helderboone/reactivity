aac4ee56a5b921110f1c6336abd085364f0fda28
Helder Boone <helder.boone@gmail.com>
2019-12-18 13:59:36 -0300

    Load indicator when get all activities

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,6 +4,7 @@ import { IActivity } from "../models/activity";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -12,6 +13,7 @@ const App = () => {
   );
 
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter(a => a.id === id)[0]);
@@ -54,8 +56,10 @@ const App = () => {
           activities.push(activity);
         })
         setActivities(activities);
-      });
+      }).then(() => setLoading(false));
   }, []);
+
+  if(loading) return <LoadingComponent content='Loading activities...' />
 
   return (
     <Fragment>
