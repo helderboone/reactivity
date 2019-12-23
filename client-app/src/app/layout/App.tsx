29138670ddb2664bff3d0d240797cfc940782d27
Helder Boone <helder.boone@gmail.com>
2019-12-23 14:49:20 -0300

    Use route to navigate between components

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import { observer } from "mobx-react-lite";
 import { Route } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ActivityForm from "../../features/activities/form/ActivityForm";
+import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 const App = () => {
   const activityStore = useContext(ActivityStore);
@@ -24,7 +25,8 @@ const App = () => {
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
         <Route exact path='/' component={HomePage} />
-        <Route path='/activities' component={ActivityDashboard} />
+        <Route exact path='/activities' component={ActivityDashboard} />
+        <Route path='/activities/:id' component={ActivityDetails} />
         <Route path='/createActivity' component={ActivityForm} />
 
       </Container>
