43c975255511cd814258daa3105e7fe3a47bf481
Helder Boone <helder.boone@gmail.com>
2019-12-26 15:26:18 -0300

    Create reusable selet input field

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,6 +8,8 @@ import { RouteComponentProps } from "react-router-dom";
 import { Form as FinalForm, Field } from "react-final-form";
 import TextInput from "../../../app/common/form/TextInput";
 import TextAreaInput from "../../../app/common/form/TextAreaInput";
+import SelectInput from "../../../app/common/form/SelectInput";
+import { category } from "../../../app/common/options/categoryOptions";
 
 interface DetailParams {
   id: string;
@@ -98,7 +100,8 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
                   name="category"
                   placeholder="Category"
                   value={activity.category}
-                  component={TextInput}
+                  component={SelectInput}
+                  options={category}
                 />
                 <Field
                   name="date"
