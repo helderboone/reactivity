dfd1dc34c741b1be6e09440ce7d481d65746fa88
Helder Boone <helder.boone@gmail.com>
2019-12-26 13:57:04 -0300

    Create reusable text input field

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,6 +6,7 @@ import ActivityStore from "../../../app/stores/activityStore";
 import { observer } from "mobx-react-lite";
 import { RouteComponentProps } from "react-router-dom";
 import { Form as FinalForm, Field } from "react-final-form";
+import TextInput from "../../../app/common/form/TextInput";
 
 interface DetailParams {
   id: string;
@@ -90,7 +91,7 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
                   name="title"
                   placeholder="Title"
                   value={activity.title}
-                  component='input'
+                  component={TextInput}
                 />
                 <Form.TextArea
                   onChange={handleInputChange}
