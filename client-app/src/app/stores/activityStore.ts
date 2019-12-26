6a0705c14452183d20774ba9da031e8c24cef5fd
Helder Boone <helder.boone@gmail.com>
2019-12-26 11:38:38 -0300

    Remove some console log

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -44,12 +44,10 @@ class ActivityStore {
         });
         this.loadingInitial = false;
       });
-      console.log(this.groupActivitiesByDate(activities));
     } catch (error) {
       runInAction("load activities error", () => {
         this.loadingInitial = false;
       });
-      console.log(error);
     }
   };
 
