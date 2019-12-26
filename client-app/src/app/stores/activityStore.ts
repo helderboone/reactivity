857731c4fc9476788a2639d5ad50282f905dac6d
Helder Boone <helder.boone@gmail.com>
2019-12-25 23:26:36 -0300

    Add routing functionality to Axios

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -69,7 +69,7 @@ class ActivityStore {
         runInAction("get activity error", () => {
           this.loadingInitial = false;
         });
-        throw error;
+        console.log(error);
       }
     }
   };
