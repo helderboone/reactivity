e9a33d4965b076b95389d94934aa1030f6b5cd8a
Helder Boone <helder.boone@gmail.com>
2019-12-31 20:05:59 -0300

    Create reusable date input field

---
+++ client-app/src/app/common/form/DateInput.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FieldRenderProps } from "react-final-form";
+import { FormFieldProps, Form, Label } from "semantic-ui-react";
+import { DateTimePicker } from "react-widgets";
+
+interface IProps extends FieldRenderProps<any, HTMLElement>, FormFieldProps {}
+
+const DateInput: React.FC<IProps> = ({
+  input,
+  width,
+  placeholder,
+  meta: { touched, error },
+  ...rest
+}) => {
+  return (
+    <Form.Field error={touched && !!error} width={width}>
+      <DateTimePicker
+        placeholder={placeholder}
+        value={input.value || null}
+        onChange={input.onChange}
+      />
+      {touched && error && (
+        <Label basic color="red">
+          {error}
+        </Label>
+      )}
+    </Form.Field>
+  );
+};
+
+export default DateInput;
