d7f3ede7662e8052fa5692fb92a1d0d4acbfc304
Helder Boone <helder.boone@gmail.com>
2020-01-03 23:49:05 -0300

    Create user controller to login

---
+++ api/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Application.User;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class UserController : BaseController
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(Login.Query query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
