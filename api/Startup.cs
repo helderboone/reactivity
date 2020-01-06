a897e48a6c1867ea51febab32cb734df65a3b4f2
Helder Boone <helder.boone@gmail.com>
2020-01-06 09:38:51 -0300

    Add JWT Generator interface and class

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using API.Middlewares;
 using Application.Activities;
+using Application.Interfaces;
 using Domain;
 using FluentValidation.AspNetCore;
+using Infrastructure.Security;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -48,6 +50,8 @@ namespace API
             var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
             identityBuilder.AddEntityFrameworkStores<DataContext>();
             identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+
+            services.AddScoped<IJwtGenerator, JwtGenerator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
