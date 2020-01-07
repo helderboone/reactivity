92212af71c17fc3bd850be5b2a2e475873274152
Helder Boone <helder.boone@gmail.com>
2020-01-07 12:47:19 -0300

    Create the typescript interface and axios methods for user

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { IActivity } from "../models/activity";
 import { history } from "../..";
 import { toast } from "react-toastify";
+import { IUser, IUserFormValues } from "../models/user";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
@@ -62,6 +63,13 @@ const Activities = {
   delete: (id: string) => requests.del(`/activities/${id}`)
 };
 
+const User = {
+  current: (): Promise<IUser> => requests.get('/user'),
+  login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
+  register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user),
+}
+
 export default {
-  Activities
+  Activities,
+  User
 };
