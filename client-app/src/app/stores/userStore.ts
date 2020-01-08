89ec4e5bb30a73942e0e4b6b3a90e139dd59a147
Helder Boone <helder.boone@gmail.com>
2020-01-08 13:16:45 -0300

    Deal with submission error logging in

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -2,6 +2,7 @@ import { observable, computed, action, runInAction } from "mobx";
 import { IUser, IUserFormValues } from "../models/user";
 import agent from "../api/agent";
 import { RootStore } from "./rootStore";
+import { history } from "../..";
 
 export default class UserStore {
   rootStore: RootStore;
@@ -23,8 +24,9 @@ export default class UserStore {
         this.user = user;
       })
       console.log(user);
+      history.push('/activities')
     } catch (error) {
-      console.log(error);
+      throw error;
     }
   };
 }
