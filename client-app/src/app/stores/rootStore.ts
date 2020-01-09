654b4fc45287593e7bf046a954cf0db023974d17
Helder Boone <helder.boone@gmail.com>
2020-01-09 13:28:56 -0300

    Add modal to login the user

--- client-app/src/app/stores/rootStore.ts
+++ client-app/src/app/stores/rootStore.ts
@@ -3,6 +3,7 @@ import UserStore from './userStore';
 import { createContext } from 'react';
 import { configure } from 'mobx';
 import CommonStore from './commonStore';
+import ModalStore from './modalStore';
 
 configure({ enforceActions: "always" });
 
@@ -12,11 +13,13 @@ export class RootStore {
         this.activityStore = new ActivityStore(this);
         this.userStore = new UserStore(this);
         this.commonStore = new CommonStore(this);
+        this.modalStore = new ModalStore(this);
     }
 
     activityStore: ActivityStore;
     userStore: UserStore;
     commonStore: CommonStore;
+    modalStore: ModalStore;
 }
 
 export const RootStoreContext = createContext(new RootStore());
--- client-app/src/features/user/LoginForm.tsx
+++ client-app/src/features/user/LoginForm.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Form as FinalForm, Field } from "react-final-form";
-import { Form, Button, Label } from "semantic-ui-react";
+import { Form, Button, Label, Header } from "semantic-ui-react";
 import TextInput from "../../app/common/form/TextInput";
 import { RootStoreContext } from "../../app/stores/rootStore";
 import { IUserFormValues } from "../../app/models/user";
@@ -26,13 +26,13 @@ const LoginForm = () => {
       render={({
         handleSubmit,
         submitting,
-        form,
         submitError,
         invalid,
         pristine,
         dirtySinceLastSubmit
       }) => (
         <Form onSubmit={handleSubmit}>
+          <Header as='h2' content='Login to Reactivities' color='teal' textAlign='center' />
           <Field name="email" component={TextInput} placeholder="Email" />
           <Field
             name="password"
@@ -43,14 +43,13 @@ const LoginForm = () => {
           {submitError && !dirtySinceLastSubmit && (
             <Label color="red" basic content={submitError.statusText} />
           )}
-          <br />
           <Button
             disabled={(invalid && !dirtySinceLastSubmit) || pristine}
             loading={submitting}
-            positive
+            color='teal'
             content="Login"
+            fluid
           />
-          <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
         </Form>
       )}
     />
