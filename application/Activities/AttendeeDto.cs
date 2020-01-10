9521ae087c053d6dc2421e8bcdfb3476c070d5e0
Helder Boone <helder.boone@gmail.com>
2020-01-09 22:29:18 -0300

    Add DTOs to shape data

---
+++ application/Activities/AttendeeDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.Activities
+{
+    public class AttendeeDto
+    {
+        public string Username { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Image { get; set; }
+
+        public bool IsHost { get; set; }
+    }
+}
