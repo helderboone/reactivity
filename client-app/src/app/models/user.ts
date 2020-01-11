adae587bf879020de708f8d8dd32b3ba3e19395c
Helder Boone <helder.boone@gmail.com>
2020-01-11 20:11:06 -0300

    Adjust username field model

--- client-app/src/app/models/user.ts
+++ client-app/src/app/models/user.ts
@@ -1,13 +1,13 @@
 export interface IUser {
-    username: string;
-    displayName: string;
-    token: string;
-    image?: string;
+  userName: string;
+  displayName: string;
+  token: string;
+  image?: string;
 }
 
 export interface IUserFormValues {
-    email: string;
-    password: string;
-    displayName?: string;
-    username?: string;
+  email: string;
+  password: string;
+  displayName?: string;
+  username?: string;
 }
