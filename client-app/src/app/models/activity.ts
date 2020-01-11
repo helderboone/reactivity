aee195e887dac66aaa51dab27a084609fa744089
Helder Boone <helder.boone@gmail.com>
2020-01-11 17:25:40 -0300

    Add attendees to list items

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -6,6 +6,7 @@ export interface IActivity {
   date: Date;
   city: string;
   venue: string;
+  attendees: IAttendee[]
 }
 
 export interface IActivityFormValues extends Partial<IActivity> {
@@ -22,10 +23,17 @@ export class ActivityFormValues implements IActivityFormValues {
   city: string = "";
   venue: string = "";
 
-  constructor(init?: IActivityFormValues ){
-    if(init && init.date){
+  constructor(init?: IActivityFormValues) {
+    if (init && init.date) {
       init.time = init.date;
     }
     Object.assign(this, init);
   }
+}
+
+export interface IAttendee {
+  username: string;
+  displayName: string;
+  image: string;
+  isHost: boolean;
 }
