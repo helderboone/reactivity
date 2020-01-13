dc24f74f7043be30fa25482c823571c895e00a97
Helder Boone <helder.boone@gmail.com>
2020-01-13 11:09:18 -0300

    Add user profile feature

---
+++ api/Controllers/ProfilesController.cs
@@ -0,0 +1,15 @@
+﻿using Application.Profiles;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ProfilesController : BaseController
+    {
+        [HttpGet("{username}")]
+        public async Task<ActionResult<Profile>> Get(string username)
+        {
+            return await Mediator.Send(new Details.Query { Username = username });
+        }
+    }
+}
