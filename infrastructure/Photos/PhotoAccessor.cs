d1f0aca4d0cd052c737107a233b079de2a389884
Helder Boone <helder.boone@gmail.com>
2020-01-13 10:48:51 -0300

    Add transformation and check if exist error sending file to cloudinary

--- infrastructure/Photos/PhotoAccessor.cs
+++ infrastructure/Photos/PhotoAccessor.cs
@@ -28,12 +28,16 @@ namespace Infrastructure.Photos
                 {
                     var uploadParams = new ImageUploadParams
                     {
-                        File = new FileDescription(file.FileName, stream)
+                        File = new FileDescription(file.FileName, stream),
+                        Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face")
                     };
 
                     uploadResult = _cloudinary.Upload(uploadParams);
                 }
             }
+
+            if (uploadResult.Error != null)
+                throw new Exception(uploadResult.Error.Message);
 
             return new PhotoUploadResult
             {
