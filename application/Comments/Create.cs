a35a7e8743be052be87dd26c6c9324361a35b77c
Helder Boone <helder.boone@gmail.com>
2020-01-15 10:12:00 -0300

    Add create comment handler

---
+++ application/Comments/Create.cs
@@ -0,0 +1,62 @@
+﻿using Application.Errors;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Comments
+{
+    public class Create
+    {
+        public class Command : IRequest<CommentDto>
+        {
+            public string Body { get; set; }
+
+            public Guid ActivityId { get; set; }
+
+            public string Username { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, CommentDto>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<CommentDto> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.ActivityId);
+
+                if (activity == null) throw new RestException(HttpStatusCode.NotFound, new { Activity = "Not found" });
+
+                var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == request.Username);
+
+                var comment = new Comment
+                {
+                    Author = user,
+                    Activity = activity,
+                    Body = request.Body,
+                    CreatedAt = DateTime.Now
+                };
+
+                activity.Comments.Add(comment);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (!success) throw new Exception("Problem saving changes");
+
+                return _mapper.Map<CommentDto>(comment);
+            }
+        }
+    }
+}
