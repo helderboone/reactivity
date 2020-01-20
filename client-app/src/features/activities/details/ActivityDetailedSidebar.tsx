bb435fdaffa953786341efb06571865840d6a228
Helder Boone <helder.boone@gmail.com>
2020-01-20 13:27:03 -0300

    Add ui component to show followed users

--- client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
+++ client-app/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -41,7 +41,7 @@ const ActivityDetailedSidebar: React.FC<IPros> = ({ attendess }) => {
                     {attendee.displayName}
                   </Link>
                 </Item.Header>
-                <Item.Extra style={{ color: "orange" }}>Following</Item.Extra>
+                {attendee.following && <Item.Extra style={{ color: "orange" }}>Following</Item.Extra> }
               </Item.Content>
             </Item>
           ))}
