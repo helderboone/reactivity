e1e89bb91e4796fc2b43aa7c785bafc31870c828
Helder Boone <helder.boone@gmail.com>
2020-01-21 10:27:00 -0300

    Add infinite scrolling to activity dashboard

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,21 +1,27 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Grid, Button } from "semantic-ui-react";
+import { Grid, Loader } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import { observer } from "mobx-react-lite";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { RootStoreContext } from "../../../app/stores/rootStore";
+import InfiniteScroll from "react-infinite-scroller";
 
 const ActivityDashboard: React.FC = () => {
-
   const rootStore = useContext(RootStoreContext);
-  const {loadActivities, loadingInitial, setPage, page, totalPages} = rootStore.activityStore
+  const {
+    loadActivities,
+    loadingInitial,
+    setPage,
+    page,
+    totalPages
+  } = rootStore.activityStore;
   const [loadingNext, setLoadingNext] = useState(false);
 
   const handleGetNext = () => {
     setLoadingNext(true);
     setPage(page + 1);
     loadActivities().then(() => setLoadingNext(false));
-  }
+  };
 
   useEffect(() => {
     loadActivities();
@@ -27,11 +33,20 @@ const ActivityDashboard: React.FC = () => {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <ActivityList />
-        <Button floated='right' content='More...' positive onClick={handleGetNext} loading={loadingNext} disabled={totalPages === page + 1} />
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={handleGetNext}
+          hasMore={!loadingNext && page + 1 < totalPages}
+          initialLoad={false}
+        >
+          <ActivityList />
+        </InfiniteScroll>
       </Grid.Column>
       <Grid.Column width={6}>
         <h2>Activity filters</h2>
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <Loader active={loadingNext} />
       </Grid.Column>
     </Grid>
   );
