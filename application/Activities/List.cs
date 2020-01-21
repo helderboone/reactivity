3f269dc0a524a219e66ff376d5bacbee3f2c0028
Helder Boone <helder.boone@gmail.com>
2020-01-21 09:59:41 -0300

    Page activity list

--- application/Activities/List.cs
+++ application/Activities/List.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -11,9 +12,26 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<List<ActivityDto>> { }
+        public class ActivitiesEnvelope
+        {
+            public List<ActivityDto> Activities { get; set; }
 
-        public class Handler : IRequestHandler<Query, List<ActivityDto>>
+            public int ActivityCount { get; set; }
+        }
+        public class Query : IRequest<ActivitiesEnvelope>
+        {
+            public Query(int? limit, int? offset)
+            {
+                Limit = limit;
+                Offset = offset;
+            }
+
+            public int? Limit { get; set; }
+
+            public int? Offset { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, ActivitiesEnvelope>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -24,11 +42,17 @@ namespace Application.Activities
                 _mapper = mapper;
             }
 
-            public async Task<List<ActivityDto>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<ActivitiesEnvelope> Handle(Query request, CancellationToken cancellationToken)
             {
-                var activities = await _context.Activities.ToListAsync();
+                var queryable = _context.Activities.AsQueryable();
 
-                return _mapper.Map<List<Activity>, List<ActivityDto>>(activities);
+                var activities = await queryable.Skip(request.Offset ?? 0).Take(request.Limit ?? 3).ToListAsync();
+
+                return new ActivitiesEnvelope
+                {
+                    Activities = _mapper.Map<List<Activity>, List<ActivityDto>>(activities),
+                    ActivityCount = queryable.Count()
+                };
             }
         }
     }
